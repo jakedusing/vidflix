a731492f48547cafd38ff0d0744f4082644a3ec2
Jake Dusing <dusingjake@gmail.com>
2023-06-28 08:41:36 -0500

    finish backend

--- src/Requests.js
+++ src/Requests.js
@@ -1,11 +1,12 @@
-const key = 'e50045e6c3a8cd2dc0a9e73c186cc1e4';
+const key = "e50045e6c3a8cd2dc0a9e73c186cc1e4";
 
 const requests = {
-    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
-    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
-  };
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestStarWars: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=star+wars&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  requestAvengers: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=avengers&page=1&include_adult=false`,
+};
 
-  export default requests
+export default requests;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,8 @@ const Home = () => {
       <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
       <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
       <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
-      <Row rowID="5" title="Horror" fetchURL={requests.requestHorror} />
+      <Row rowID="5" title="Star Wars" fetchURL={requests.requestStarWars} />
+      <Row rowID="6" title="Avengers" fetchURL={requests.requestAvengers} />
     </div>
   );
 };
